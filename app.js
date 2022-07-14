5b3d8206047b697a1c8c36e081e9499377bd30e6
JosephCass <josephcassinera@gmail.com>
2022-07-13 22:12:33 -0400

    Added rainbow button functionality to turn the drawing color rainbow when clicked

--- app.js
+++ app.js
@@ -1,23 +1,41 @@
 let gridContainer = document.querySelector(".grid");
 let gridSizeSlider = document.querySelector(".grid-size-slider");
 let gridSizeLabel = document.querySelector(".grid-size-label");
+let raindbowBtn = document.querySelector(".rainbow-btn");
 
+//Create Rows and Set Drawing Color
 function makeRows(rows, cols) {
   gridContainer.style.setProperty("--grid-rows", rows);
   gridContainer.style.setProperty("--grid-cols", cols);
   for (let c = 0; c < rows * cols; c++) {
     let cell = document.createElement("div");
-    // cell.innerText = c + 1;
     gridContainer.appendChild(cell).className = "grid-item";
   }
   let gridItem = document.querySelectorAll(".grid-item");
 
   gridItem.forEach(function (item) {
     item.addEventListener("mouseover", function (e) {
-      e.target.style.backgroundColor = "green";
+      e.target.style.backgroundColor = drawingColor();
     });
   });
 }
+
+function setDeafult() {
+  return "black";
+}
+
+function rainbow() {
+  let randomR = Math.floor(Math.random() * 256);
+  const randomG = Math.floor(Math.random() * 256);
+  const randomB = Math.floor(Math.random() * 256);
+  return `rgb(${randomR}, ${randomG}, ${randomB})`;
+}
+
+let drawingColor = setDeafult;
+
+raindbowBtn.addEventListener("click", function () {
+  drawingColor = rainbow;
+});
 
 let gridSize = 33;
 gridSizeLabel.textContent = `${gridSizeSlider.value} X ${gridSizeSlider.value}`;
